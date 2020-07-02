7a8d75d2d9ef1e5e80ba9b5a7ec8125f84e7e213
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-07-03 01:19:11 +0200

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,8 +1,12 @@
 variable "name_ip_public" {
   type        = string
+}
 variable "location" {
   type        = string
+}
 variable "public_ip_address_allocation" {
   type        = string
+}
 variable "resource_group_name" {
   type        = string
+}
