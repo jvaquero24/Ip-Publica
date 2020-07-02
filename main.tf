bd1930315eb68ad284f5fef583bc82154cc53c12
jvaquero24 <67736596+jvaquero24@users.noreply.github.com>
2020-07-03 01:47:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+data "azurerm_resource_group" "RG" {
+  name = var.resource_group_name
+}
+
 resource "azurerm_public_ip" "ippublic" {
   name                         = var.name_ip_public
   location                     = var.location
-  resource_group_name          = var.resource_group_name
+  resource_group_name          = data.azurerm_resource_group.RG.name
   public_ip_address_allocation = var.public_ip_address_allocation
 
 }
